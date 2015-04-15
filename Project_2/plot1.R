91a6f002eb442bb3afdf416215e204173fd7cd30
zosiek23 <zosiek23@hotmail.com>
2015-04-15 21:04:24 +0100

    Plot 1 and 2 done

--- Project_2/plot1.R
+++ Project_2/plot1.R
@@ -1,14 +1,21 @@
 # Loads RDS
-NEI <- readRDS("data/summarySCC_PM25.rds")
-SCC <- readRDS("data/Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
 # Samples data for testing
 NEIsample <- NEI[sample(nrow(NEI), size = 1000, replace = F), ]
 
-# Aggregates
+# Aggregate emmision for each year
 Emissions <- aggregate(NEI[, 'Emissions'], by = list(NEI$year), FUN = sum)
-Emissions$PM <- round(Emissions[, 2] / 1000, 2)
+print(Emissions)
+Emissions$ROUND <- round(Emissions[, 2] / 1000, 2)
 
-png(filename = "plot1.png")
-barplot(Emissions$PM, names.arg = Emissions$Group.1, main = expression('Total Emission of PM'[2.5]), xlab = 'Year', ylab = expression(paste('PM', ''[2.5], ' in Kilotons')))
+# Open device
+png(filename='plot1.png',width=480,height=480,units='px')
+
+# Plot the data
+barplot(Emissions$ROUND, names.arg = Emissions$Group.1, main = expression('Total Emission of PM'[2.5]), xlab = 'Year', ylab = expression(paste('PM', ''[2.5], ' in Kilotons')), col='red')
+
+# Turn off device
 dev.off()
+
